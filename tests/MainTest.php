54160e7823b56828cdb8f6a16044f36e0979b2d1
Jakub Miskech <yakub@Jakubs-MacBook-Pro.local>
2020-05-27 10:44:03 +0200

    Add tests background, travis

---
+++ tests/MainTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use \Yakub\Yxel\Main;
+
+final class MainTest extends TestCase {
+
+    public function testTodo(): void {
+        $this->assertEquals(
+            'Hi',
+            'Hi'
+        );
+    }
+}
